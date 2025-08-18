54d57ab560194bbb282faf58a7b3f240ed2c7770
sujit Singh <fishermansujit@gmail.com>
2025-08-18 08:57:43 +0545

    register and login of amdin and user sucessfully added

--- vite-project/src/Pages/Login.jsx
+++ vite-project/src/Pages/Login.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import LoginForm from '../components/LoginForm'
+import EmployerLogin from '../components/EmployerLogin'
 
 const Login = () => {
   return (
+   <>
     <div><LoginForm/></div>
+    <EmployerLogin/>
+   </>
   )
 }
 
--- vite-project/src/Pages/Register.jsx
+++ vite-project/src/Pages/Register.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import RegisterForm from '../components/RegisterForm'
+import EmployerRegister from '../components/EmployerRegister'
 
 const Register = () => {
   return (
+    <>
     <div><RegisterForm/></div>
+    <EmployerRegister/>
+    </>
+
   )
 }
 
