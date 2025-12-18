e908604ba041d9c352e8add3f2a32c161fc11017
Anuj12pokharel <anujpokharel.076@kathford.edu.np>
2025-12-18 09:54:18 +0545

    search bar option

--- job-backend/routes/AdminRoutes.ts
+++ job-backend/routes/AdminRoutes.ts
@@ -9,7 +9,8 @@ import {
   deleteUser,
   deleteEmployer,
   updateUserByAdmin,
-  updateEmployerByAdmin
+  updateEmployerByAdmin,
+  verifyEmployer
 } from "../controller/AdminController";
 import { protect } from "../middleware/authMiddleware";
 import uploadProfile from "../middleware/uploadProfile";
@@ -27,5 +28,6 @@ router.delete("/user/:id", protect, deleteUser);
 router.delete("/employer/:id", protect, deleteEmployer);
 router.put("/user/:id", protect, uploadProfile.single("profilePicture"), updateUserByAdmin);
 router.put("/employer/:id", protect, uploadProfile.single("profilePicture"), updateEmployerByAdmin);
+router.put("/verify-employer/:id", protect, verifyEmployer);
 
 export default router;
