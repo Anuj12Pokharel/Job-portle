ed6affe5869ad4b223d42169f65e2b7b11ba9da0
Anjali Singh <Anjalisingh44>
2026-02-22 00:20:54 +0545

    feature updated

--- job-backend/routes/ServiceInquiry.ts
+++ job-backend/routes/ServiceInquiry.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { createServiceInquiry } from "../controller/ServiceInquiry";
+import { createServiceInquiry, getServiceInquiries } from "../controller/ServiceInquiry";
+import { protect } from "../middleware/authMiddleware";
+import checkAdmin from "../middleware/checkAdmin";
 
 const router = Router();
 
 router.post("/service-inquiry", createServiceInquiry);
+router.get("/all", protect, checkAdmin, getServiceInquiries);
 
 export default router;
