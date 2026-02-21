b12b988301df0eabf2cf7a7292bb1719310e0229
Anjali Singh <Anjalisingh44>
2026-02-22 00:22:09 +0545

    feature updated

--- job-backend/routes/userFormRoutes.ts
+++ job-backend/routes/userFormRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import upload from "../middleware/upload";
-import { submitForm } from "../controller/userFormController";
+import { submitForm, getAllForms } from "../controller/userFormController";
 import { protect } from "../middleware/authMiddleware";
+import checkAdmin from "../middleware/checkAdmin";
 
 const router = Router();
 
 router.post("/submit-form", protect, upload.single("resume"), submitForm);
+router.get("/all", protect, checkAdmin, getAllForms);
 
 export default router;
